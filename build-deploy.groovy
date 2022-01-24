733a783caedf13e32f86353e56de981f067cfbdc
K CHaitu <kchaithanya.2010@gmail.com>
2022-01-24 23:28:29 +0530

    sample code

---
+++ build-deploy.groovy
@@ -0,0 +1,31 @@
+// declarative pipeline
+pipeline{
+    agent any
+    stages{
+        stage("clone the code from git hub"){
+            steps{
+                println("here clonining the code from github")
+            }
+        }
+        stage("buid the code"){
+            steps{
+                println("here building the code using maven tool")
+            }
+        }
+        stage("upload the artifacts"){
+            steps{
+                println("here uploading the artifacts to s3")
+            }
+        }
+        stage("download artifacts"){
+            steps{
+                println("downloading artifacts from s3")
+            }
+        }
+        stage("copy to other servers"){
+            steps{
+                println("coping artifacts to other server")
+            }
+        }
+    }
+}
